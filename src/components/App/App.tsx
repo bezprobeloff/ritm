54c91f94904f12bc41877e60d5c200932b6589c7
Mikhail Belov <bezprobeloff@gmail.com>
2023-03-14 11:57:26 +0300

    refact: перенос компнентов в Main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,25 +3,12 @@ import './App.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
-import Techs from '../Techs/Techs';
-import Dev from '../Dev/Dev';
-import Process from '../Process/Process';
-import Feedback from '../Feedback/Feedback';
-import FAQ from '../FAQ/FAQ';
-import Contacts from '../Contacts/Contacts';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main>
-        <Techs />
-        <Dev />
-        <Process />
-        <Feedback />
-        <FAQ />
-        <Contacts />
-      </Main>
+      <Main />
       <Footer />
     </div>
   );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,23 @@
 import './Main.scss';
 import React from 'react';
+import Techs from '../Techs/Techs';
+import Dev from '../Dev/Dev';
+import Process from '../Process/Process';
+import Feedback from '../Feedback/Feedback';
+import FAQ from '../FAQ/FAQ';
+import Contacts from '../Contacts/Contacts';
 
-type Props = {
-  children: React.ReactNode;
-};
-const Main: React.FC<Props> = ({ children }) => {
-  return <main className="main">{children}</main>;
+const Main: React.FC = () => {
+  return (
+    <main className="main">
+      <Techs />
+      <Dev />
+      <Process />
+      <Feedback />
+      <FAQ />
+      <Contacts />
+    </main>
+  );
 };
 
 export default Main;

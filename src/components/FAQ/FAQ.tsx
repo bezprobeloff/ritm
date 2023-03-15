8c66df15b9c860023a4d45b5b564dec4e8a704c4
Mikhail Belov <bezprobeloff@gmail.com>
2023-03-15 10:53:04 +0300

    refact: данные faq выделены в отдельный файл

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,55 +1,26 @@
 import './FAQ.scss';
 import React from 'react';
+import { FAQ_ITEMS } from './constants';
 
 const FAQ: React.FC = () => {
+  const faqItems = FAQ_ITEMS.map((item, index) => (
+    <details key={index} open={item.isOpened} className="faq__details">
+      <summary className="faq__question">
+        <span className="faq__question-icon" />
+        <p className="faq__question-text">{item.questionText}</p>
+      </summary>
+      <p className="faq__answer">{item.answerText}</p>
+    </details>
+  ));
+
+  const faqListElement = <div className="faq__list">{faqItems}</div>;
+
   return (
     <section className="section faq">
       <div className="section__wrapper">
         <h2 className="section__title">F.A.Q</h2>
         <h3 className="section__subtitle faq__subtitle">Частые вопросы</h3>
-        <div className="faq__list">
-          <details open={true} className="faq__details">
-            <summary className="faq__question">
-              <span className="faq__question-icon" />
-              <p className="faq__question-text">
-                Если мне нужен простой сайт, я могу сделаю его с помощью шаблона. Зачем мне сложное
-                веб-приложение?
-              </p>
-            </summary>
-            <p className="faq__answer">
-              Если мне нужен простой сайт, я сделаю его на конструкторе. Зачем мне все эти сложности
-              с веб-приложением?
-            </p>
-          </details>
-          <details className="faq__details">
-            <summary className="faq__question">
-              <span className="faq__question-icon" />
-              <p className="faq__question-text">
-                Мне предлагают сделать сайт на Bitrix и Wordpress. Эти решения хуже и не будут
-                работать?
-              </p>
-            </summary>
-            <p className="faq__answer">%ДОБАВИТЬ_ТЕКСТ%</p>
-          </details>
-          <details className="faq__details">
-            <summary className="faq__question">
-              <span className="faq__question-icon" />
-              <p className="faq__question-text">
-                Можно ли разбить процесс на этапы и реализовывать их последовательно?
-              </p>
-            </summary>
-            <p className="faq__answer">%ДОБАВИТЬ_ТЕКСТ%</p>
-          </details>
-          <details className="faq__details">
-            <summary className="faq__question">
-              <span className="faq__question-icon" />
-              <p className="faq__question-text">
-                У меня уже есть сайт, он прекрасно работает. Мне все придется делать заново?
-              </p>
-            </summary>
-            <p className="faq__answer">%ДОБАВИТЬ_ТЕКСТ%</p>
-          </details>
-        </div>
+        {faqListElement}
       </div>
     </section>
   );

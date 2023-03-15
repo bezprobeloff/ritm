ca0e204c236065d2e21c5622b41185e98e244884
Mikhail Belov <bezprobeloff@gmail.com>
2023-03-15 11:28:13 +0300

    refact: данные web выделены в отдельный файл

--- src/components/Techs/Web/Web.tsx
+++ src/components/Techs/Web/Web.tsx
@@ -1,30 +1,25 @@
 import '../../../styles/section.scss';
 import './Web.scss';
 import React from 'react';
+import { WEB_ITEMS } from './constants';
 
 const Web: React.FC = () => {
+  const webItems = WEB_ITEMS.map((item, index) => (
+    <li key={index} className="section__item">
+      <p className="web__item-title">{item.title}</p>
+      <p className="web__item-description">{item.description}</p>
+    </li>
+  ));
+
+  const webListElement = <ul className="section__list">{webItems}</ul>;
+
   return (
     <div className="web">
       <h3 className="section__subtitle web__title">
         Веб-приложение – это программный продукт, работающий в онлайн-среде и состоящий из двух
         основных частей
       </h3>
-      <ul className="section__list">
-        <li className="section__item">
-          <p className="web__item-title">Клиентская часть</p>
-          <p className="web__item-description">
-            Это интерфейс, через который пользователь управляет приложением. Самый простой и
-            распространенный пример клиентской части веб-приложения – корпоративный сайт компании.
-          </p>
-        </li>
-        <li className="section__item">
-          <p className="web__item-title">Серверная часть</p>
-          <p className="web__item-description">
-            Это вычислительный и логический центр веб-приложения. Примером серверной части служит
-            система управления заказами интернет-магазина.
-          </p>
-        </li>
-      </ul>
+      {webListElement}
     </div>
   );
 };

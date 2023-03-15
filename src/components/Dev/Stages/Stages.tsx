af5699b06710ab3a099986c00916159317dcaab5
Mikhail Belov <bezprobeloff@gmail.com>
2023-03-15 10:01:14 +0300

    refact: данные stages выделены в отдельный файл

--- src/components/Dev/Stages/Stages.tsx
+++ src/components/Dev/Stages/Stages.tsx
@@ -1,98 +1,41 @@
 import '../../../styles/section.scss';
 import './Stages.scss';
-import imageStep1 from '../../../images/image-step1.svg';
-import imageStep2 from '../../../images/image-step2.svg';
-import imageStep3 from '../../../images/image-step3.svg';
-import imageStep4 from '../../../images/image-step4.svg';
 import React from 'react';
+import { STAGES } from './constants';
 
 const Stages: React.FC = () => {
+  const stageItems = STAGES.map((item, indexItem) => {
+    const descriptionElements = item.descriptions.map((text, indexText) => (
+      <p key={'text' + indexText} className="stages__item-description">
+        {text}
+      </p>
+    ));
+
+    return (
+      <li key={'item' + indexItem} className="stages__item">
+        <div className="stages__item-text-wrap">
+          <p className="stages__item-title">{item.title}</p>
+          {descriptionElements}
+        </div>
+        <div className="stages__item-image-wrapper">
+          <img
+            className="stages__item-image stages__item-image_type_first"
+            src={item.imgSrc}
+            alt={item.imgAlt}
+          />
+        </div>
+      </li>
+    );
+  });
+
+  const stageListElement = <ul className="stages__list">{stageItems}</ul>;
+
   return (
     <div className="stages">
       <h3 className="section__subtitle stages__title">
         Интеграция IT-решений в&nbsp;ваш&nbsp;бизнес
       </h3>
-      <ul className="stages__list">
-        <li className="stages__item">
-          <div className="stages__item-text-wrap">
-            <p className="stages__item-title">Первый этап</p>
-            <p className="stages__item-description">
-              Ваша компания только начинает свой путь развития. Вам необходим простой сайт-визитка
-              или одностраничный лэндинг для продвижения продукта.
-            </p>
-            <p className="stages__item-description">
-              С помощью технологии веб-приложения мы разрабатываем быстрый сайт с современным
-              адаптивным дизайном.
-            </p>
-          </div>
-          <div className="stages__item-image-wrapper">
-            <img
-              className="stages__item-image stages__item-image_type_first"
-              src={imageStep1}
-              alt="Картинка устройств"
-            />
-          </div>
-        </li>
-        <li className="stages__item">
-          <div className="stages__item-text-wrap">
-            <p className="stages__item-title">Второй этап</p>
-            <p className="stages__item-description">
-              Ваши бизнес-процессы становятся все сложнее и требуют новых функциональных
-              возможностей. Мы добавляем на сайт каталог товаров с системой уведомления менеджеров.
-            </p>
-            <p className="stages__item-description">
-              Оба блока работают в единой экосистеме и не конфликтуют между собой.
-            </p>
-          </div>
-          <div className="stages__item-image-wrapper">
-            <img
-              className="stages__item-image stages__item-image_type_second"
-              src={imageStep2}
-              alt="Картинка устройств"
-            />
-          </div>
-        </li>
-        <li className="stages__item">
-          <div className="stages__item-text-wrap">
-            <p className="stages__item-title">Третий этап</p>
-            <p className="stages__item-description">
-              Вы решаете сделать шаг в сторону полноценного e-commerce и запустить интернет-магазин.
-              Мы разрабатываем систему приема платежей, авторизацию и гео-треккинг.
-            </p>
-            <p className="stages__item-description">
-              Все разделы вашего корпоративного портала работают вместе над реализацией единой
-              задачи.
-            </p>
-          </div>
-          <div className="stages__item-image-wrapper">
-            <img
-              className="stages__item-image stages__item-image_type_third"
-              src={imageStep3}
-              alt="Картинка устройств"
-            />
-          </div>
-        </li>
-        <li className="stages__item">
-          <div className="stages__item-text-wrap">
-            <p className="stages__item-title">Четвертый этап</p>
-            <p className="stages__item-description">
-              Ваша компания внедряет системы учета, CRM-модули, контроль эффективности менеджеров и
-              продвинутая бизнес-аналитика. Вам требуется создание своей бизнес-экосистемы.
-            </p>
-            <p className="stages__item-description">
-              Мы продолжаем процесс разработки и создаём уникальный продукт, связывающий все ресурсы
-              компании.
-            </p>
-          </div>
-          <div className="stages__item-image-wrapper">
-            <img
-              className="stages__item-image stages__item-image_type_fourth"
-              src={imageStep4}
-              alt="Картинка устройств"
-            />
-          </div>
-        </li>
-      </ul>
+      {stageListElement}
     </div>
   );
 };
---
+++ src/components/Dev/Stages/constants.ts
@@ -0,0 +1,63 @@
+import imageStep1 from '../../../images/image-step1.svg';
+import imageStep2 from '../../../images/image-step2.svg';
+import imageStep3 from '../../../images/image-step3.svg';
+import imageStep4 from '../../../images/image-step4.svg';
+interface TStage {
+  title: string;
+  descriptions: string[];
+  imgClass: string;
+  imgSrc: string;
+  imgAlt: string;
+}
+
+const STAGES: TStage[] = [
+  {
+    title: 'Первый этап',
+    descriptions: [
+      'Ваша компания только начинает свой путь развития. Вам необходим простой сайт-визитка' +
+        ' или одностраничный лэндинг для продвижения продукта.',
+      'С помощью технологии веб-приложения мы разрабатываем быстрый сайт с современным' +
+        ' адаптивным дизайном.'
+    ],
+    imgClass: 'stages__item-image_type_first',
+    imgSrc: imageStep1,
+    imgAlt: 'Картинка устройств'
+  },
+  {
+    title: 'Второй этап',
+    descriptions: [
+      'Ваши бизнес-процессы становятся все сложнее и требуют новых функциональных' +
+        ' возможностей. Мы добавляем на сайт каталог товаров с системой уведомления менеджеров.',
+      'Оба блока работают в единой экосистеме и не конфликтуют между собой.'
+    ],
+    imgClass: 'stages__item-image_type_second',
+    imgSrc: imageStep2,
+    imgAlt: 'Картинка устройств'
+  },
+  {
+    title: 'Третий этап',
+    descriptions: [
+      'Вы решаете сделать шаг в сторону полноценного e-commerce и запустить интернет-магазин.' +
+        ' Мы разрабатываем систему приема платежей, авторизацию и гео-треккинг.',
+      'Все разделы вашего корпоративного портала работают вместе над реализацией единой' +
+        ' задачи.'
+    ],
+    imgClass: 'stages__item-image_type_third',
+    imgSrc: imageStep3,
+    imgAlt: 'Картинка корзины'
+  },
+  {
+    title: 'Четвертый этап',
+    descriptions: [
+      'Ваша компания внедряет системы учета, CRM-модули, контроль эффективности менеджеров и' +
+        ' продвинутая бизнес-аналитика. Вам требуется создание своей бизнес-экосистемы.',
+      'Мы продолжаем процесс разработки и создаём уникальный продукт, связывающий все ресурсы' +
+        ' компании.'
+    ],
+    imgClass: 'stages__item-image_type_fourth',
+    imgSrc: imageStep4,
+    imgAlt: 'Картинка отчетов'
+  }
+];
+
+export { STAGES };

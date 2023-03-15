24228688367f89498a62299762001ce795c7b13d
Mikhail Belov <bezprobeloff@gmail.com>
2023-03-15 10:28:43 +0300

    refact: данные tools выделены в отдельный файл

--- src/components/Dev/Tools/Tools.tsx
+++ src/components/Dev/Tools/Tools.tsx
@@ -1,45 +1,22 @@
 import './Tools.scss';
-import iconReact from '../../../images/icon-react.svg';
-import iconNodeJS from '../../../images/icon-nodejs.svg';
-import iconNestJS from '../../../images/icon-nestjs.svg';
-import iconNginx from '../../../images/icon-nginx.svg';
 import React from 'react';
+import { TOOLS } from './constants';
 
 const Tools: React.FC = () => {
-  return (
-    <div className="tools">
-      <div className="tools__list">
-        <div className="tools__item">
-          <img
-            className="tools__item-image tools__item-image_type_react"
-            src={iconReact}
-            alt="React"
-          />
-        </div>
-        <div className="tools__item">
-          <img
-            className="tools__item-image tools__item-image_type_nodejs"
-            src={iconNodeJS}
-            alt="NodeJs"
-          />
-        </div>
-        <div className="tools__item">
-          <img
-            className="tools__item-image tools__item-image_type_nest"
-            src={iconNestJS}
-            alt="Nest"
-          />
-        </div>
-        <div className="tools__item">
-          <img
-            className="tools__item-image tools__item-image_type_nginx"
-            src={iconNginx}
-            alt="Nginx"
-          />
-        </div>
-      </div>
+  const toolItems = TOOLS.map((item, index) => (
+    <div key={index} className="tools__item">
+      <img
+        className={`tools__item-image  ${item.imgClass}`}
+        src={item.imgSrc}
+        alt={item.imgAlt}
+        title={item.imgAlt}
+      />
     </div>
-  );
+  ));
+
+  const toolListElement = <ul className="tools__list">{toolItems}</ul>;
+
+  return <div className="tools">{toolListElement}</div>;
 };
 
 export default Tools;
---
+++ src/components/Dev/Tools/constants.ts
@@ -0,0 +1,35 @@
+import iconReact from '../../../images/icon-react.svg';
+import iconNodeJS from '../../../images/icon-nodejs.svg';
+import iconNestJS from '../../../images/icon-nestjs.svg';
+import iconNginx from '../../../images/icon-nginx.svg';
+
+interface TTool {
+  imgClass: string;
+  imgSrc: string;
+  imgAlt: string;
+}
+
+const TOOLS: TTool[] = [
+  {
+    imgClass: 'tools__item-image_type_react',
+    imgSrc: iconReact,
+    imgAlt: 'React'
+  },
+  {
+    imgClass: 'tools__item-image_type_nodejs',
+    imgSrc: iconNodeJS,
+    imgAlt: 'NodeJs'
+  },
+  {
+    imgClass: 'tools__item-image_type_nest',
+    imgSrc: iconNestJS,
+    imgAlt: 'NestJS'
+  },
+  {
+    imgClass: 'tools__item-image_type_nginx',
+    imgSrc: iconNginx,
+    imgAlt: 'Nginx'
+  }
+];
+
+export { TOOLS };

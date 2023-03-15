80845ae8ae2266d33506370f1b70f124527f1460
Mikhail Belov <bezprobeloff@gmail.com>
2023-03-15 17:04:50 +0300

    feat: сверстан попап

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import Popup from '../Popup/Popup';
 
 function App() {
+  const [isPopupOpened, setIsPopupOpened] = useState(false);
+  const closePopup = () => {
+    setIsPopupOpened(false);
+  };
+
   return (
     <div className="App">
       <Header />
       <Main />
       <Footer />
+      <Popup isOpen={isPopupOpened} onClose={closePopup}>
+        <p className="popup__title">Спасибо!</p>
+        <p className="popup__message">Ваша заявка принята. Мы свяжемся с вами в ближайшее время.</p>
+      </Popup>
     </div>
   );
 }
---
+++ src/components/Popup/Popup.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import './Popup.scss';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void | React.MouseEvent;
+  children: React.ReactNode;
+};
+
+const Popup: React.FC<Props> = ({ isOpen, onClose, children }) => {
+  const classPopupOpened = `${isOpen ? 'popup_opened' : ''}`;
+  const handleOverlayClose = (evt: React.MouseEvent) => {
+    return evt.target === evt.currentTarget && onClose();
+  };
+
+  const handleEscClosePopup = (evt: KeyboardEvent) => {
+    if (evt.key !== 'Escape') return;
+
+    onClose();
+  };
+  const setHandleEscClosePopup = () => {
+    document.addEventListener('keydown', handleEscClosePopup);
+  };
+  const removeHandleEscClosePopup = () => {
+    document.removeEventListener('keydown', handleEscClosePopup);
+  };
+
+  useEffect(() => {
+    return isOpen ? setHandleEscClosePopup() : removeHandleEscClosePopup();
+  }, [isOpen]);
+
+  return (
+    <div className={`popup ${classPopupOpened}`} onClick={handleOverlayClose}>
+      <div className="popup__container">
+        <button className="popup__button-close" onClick={onClose} type="button"></button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;

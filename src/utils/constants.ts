01864fddd0f5b2a089a57e439f8ce3e85517bd02
Dmitrii <rakhmanin.dm@gmail.com>
2023-03-29 16:29:40 +0300

    ref: add env file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
+export const { REACT_APP_API_URL: BASE_URL_API_RITM = 'https://ritm.studio/api/callback' } =
+  process.env;
 export const BASE_URL_TEST = 'https://api.publicapis.org/entries';
-export const BASE_URL_API_RITM = 'https://api.ritm.studio';
 export const PATTERN_EMAIL: string =
   '^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@' +
   '[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$';
@@ -11,5 +12,5 @@ export const PAD_WIDTH_WITHOUT = PAD_WIDTH - 1;
 export const KEYBOARD_KEYS = { ESCAPE: 'Escape' };
 export const MESSAGE_SUCCESS = {
   title: 'Спасибо!',
-  message: 'Ваша заявка принята. Мы свяжемся с вами в ближайшее время.'
+  message: 'Ваша заявка принята. Мы свяжемся с вами в ближайшее время.'
 };
